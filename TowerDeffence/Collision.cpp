317b13bc86499fe61b48cf09af208e735141934d
mniepokoj <79061647+mniepokoj@users.noreply.github.com>
2021-02-18 14:40:03 +0100

    pow(x, 2) converted to x*x

--- TowerDeffence/Collision.cpp
+++ TowerDeffence/Collision.cpp
@@ -31,8 +31,8 @@ namespace Collision
 	{
 		float distance1 = circle1.getPosition().x - circle2.getPosition().x;
 		float distance2 = circle1.getPosition().y - circle2.getPosition().y;
-		distance1 = pow(distance1, 2) + pow(distance2, 2);
-		distance2 = pow(circle1.getRadius() + circle2.getRadius(), 2);
+		distance1 = distance1 * distance1 + distance2 * distance2;
+		distance2 = (circle1.getRadius() + circle2.getRadius()) * (circle1.getRadius() + circle2.getRadius());
 		return (distance2 >= distance1);
 	}
 
@@ -40,28 +40,28 @@ namespace Collision
 	{
 		float distance1 = position1.x - position2.x;
 		float distance2 = position1.y - position2.y;
-		distance1 = pow(distance1, 2) + pow(distance2, 2);
-		distance2 = pow(radious1 + radious2, 2);
+		distance1 = distance1 * distance1 + distance2 * distance2;
+		distance2 = (radious1 + radious2) * (radious1 + radious2);
 		return (distance2 >= distance1);
 	}
 
 	bool CirclePointContain(float radius, const sf::Vector2f& position, const sf::Vector2f& point)
 	{
-		return (pow(radius, 2) >= pow(position.x - point.x, 2) + pow(position.y - point.y, 2));
+		return (radius * radius) >= (position.x - point.x)*(position.x - point.x) + (position.y - point.y) * (position.y - point.y);
 	}
 
 	bool CirclePointContain(const sf::CircleShape& circle, const sf::Vector2f& point)
 	{
-		return (pow(circle.getRadius(), 2) >= pow(circle.getPosition().x - point.x, 2) + pow(circle.getPosition().y - point.y, 2));
+		return (circle.getRadius()*circle.getRadius()) >= (circle.getPosition().x - point.x)*(circle.getPosition().x-point.x) + ((circle.getPosition().y-point.y)*(circle.getPosition().y-point.y));
 	}
 
 	bool OutlineCircleCollision(const sf::CircleShape& circle1, const sf::CircleShape& circle2)
 	{
 		float r = circle1.getRadius() + circle1.getOutlineThickness() - circle2.getRadius();
 		float R = circle1.getRadius() + circle1.getOutlineThickness() + circle2.getRadius();
-		float distance = pow(circle1.getPosition().x - circle2.getPosition().x, 2) + pow(circle1.getPosition().y - circle2.getPosition().y, 2);
+		float distance = (circle1.getPosition().x - circle2.getPosition().x)* (circle1.getPosition().x - circle2.getPosition().x) + (circle1.getPosition().y - circle2.getPosition().y)* (circle1.getPosition().y - circle2.getPosition().y);
 
-		if (pow(r, 2) <= distance && distance <= pow(R, 2))
+		if (r*r <= distance && distance <= R*R)
 		{
 			return true;
 		}
@@ -69,7 +69,7 @@ namespace Collision
 		{
 			r = circle1.getRadius() - circle2.getRadius();
 			R = circle1.getRadius() + circle2.getRadius();
-			return (pow(r, 2) <= distance && distance <= pow(R, 2));
+			return (r*r <= distance && distance <= R*R);
 		}
 	}
 }
